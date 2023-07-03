7e5ff8c3342e05bec8f95dc50e0e6a738e3b6273
Roman Bolshakov <rb812@yandex.ru>
2023-07-03 16:19:13 +0300

    fix: исправить весь стайл 2

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -5,11 +5,18 @@ import ru.yandex.practicum.filmorate.model.User;
 
 public interface UserService {
     User createUser(User user);
+
     User updateUser(User user);
+
     List<User> getAllUsers();
+
     User getUser(Integer id);
+
     void addFriend(Integer userId, Integer friendId);
+
     void deleteFriend(Integer userId, Integer friendId);
+
     List<User> getAllFriends(Integer userId);
+
     List<User> getCommonFriends(Integer id, Integer otherId);
 }
