5b209c8313ef95f26887d3fa560ac742d7188ed6
Roman Bolshakov <rb812@yandex.ru>
2023-07-05 00:00:41 +0300

    fix: исправить стайл

--- src/main/java/ru/yandex/practicum/filmorate/exception/NegativeValueException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NegativeValueException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class NegativeValueException extends RuntimeException{
+public class NegativeValueException extends RuntimeException {
     public NegativeValueException(final String message) {
         super(message);
     }
